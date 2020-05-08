e130e1ff4aa46e77ec72c53e3891cbc5dac0a00b
shfkf <shfkf@DESKTOP-DJHLVC2>
2020-05-08 17:02:06 +0900

    노트북 업로드 실험

---
+++ Chapter02/src/Ex02_01.java
@@ -0,0 +1,32 @@
+//import java.util.Scanner;
+public class Ex02_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	/*	
+		Scanner scn = new Scanner(System.in);
+		float result;
+		float a;
+		float b;
+		
+	
+		System.out.print("첫 번째 숫자를 입력하세요:");
+		a = scn.nextFloat();
+		
+		System.out.print("두 번째 숫자를 입력하세요:");
+		b = scn.nextFloat();
+
+		result = a + b;
+		System.out.println(a + "+" + b + "=" + result);
+		
+		result = a / b;
+		System.out.print(a + "/" + b + "=" + (int)result);
+		*/
+		
+		//System.out.println("hello! 김창영!");
+		
+				
+	}
+
+}
