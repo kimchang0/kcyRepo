d940ce7729ce20914b6e9789689cf5dd1f586d1b
DATA8320-10 <DATA8320-10@DESKTOP-QA30AO1>
2020-06-01 13:48:07 +0900

    공부한 것

---
+++ javaPrj/src/javaPrj/Study.java
@@ -0,0 +1,51 @@
+package javaPrj;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Study {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner scan = new Scanner(System.in);
+		int a, b;
+		char ch;
+
+		while (true) {
+			System.out.printf("더할 첫 수 입력: ");
+			a = scan.nextInt();
+			System.out.printf("더할 두번 째 수 입력: ");
+			b = scan.nextInt();
+			System.out.printf("계산할 연산자를 입력하세요: ");
+			ch = (char) System.in.read();
+
+			switch (ch) {
+			case '+':
+				//System.out.printf("%d + %d = %d 입니다. \n", a, b, a + b);
+				System.out.println(a + "+" + b + "=" + (a + b) + "입니다.");
+				break;
+			case '-':
+				System.out.printf("%d - %d = %d 입니다. \n", a, b, a - b);
+				break;
+			case '*':
+				System.out.printf("%d * %d = %d 입니다. \n", a, b, a * b);
+				break;
+			case '/':
+				System.out.printf("%d / %d = %d 입니다. \n", a, b, a / b);
+				break;
+			case '%':
+				System.out.printf("%d % %d = %d 입니다. \n", a, b, a % b);
+				break;
+			default:
+				System.out.printf("연산자를 잘 못 입력했습니다. \n");
+				break;
+				
+			}
+
+		}
+		
+		
+
+	}
+
+}
