ce7b255d98ab6d36654778c93767e03cefed57d9
kimchang0 <newshfkfk@gmail.com>
2021-03-30 00:06:53 +0900

    programers_kth_number_success

--- Study/src/kcy/programers_kth_number.java
+++ Study/src/kcy/programers_kth_number.java
@@ -8,29 +8,29 @@ public class programers_kth_number {
 
 		int [] array = {1, 5, 2, 6, 3, 7, 4};
 		int [][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-		int[] answer = new int [100000];
+		int[] answer = new int [commands.length];
 		
 			
 		for(int i = 0; i < commands.length; i++) {
 			int x = commands[i][0];
 			int y = commands[i][1];
-			int z = commands[i][2];
+			int z = commands[i][2] - 1;
 			int[] place = {};
 			int num = 0;
-			answer = new int [100000];
-			place = new int [10000000];
-			
+			for(int j = x-1; j <= y-1; j++) {
+				num++;
+			}
+			place = new int [num];
+			num = 0;
 			for(int j = x-1; j <= y-1; j++) {
 				place[num] = array[j];
 				System.out.println(place[num]);
 				num++;
 			}
-			for(int w = 0; w < 3; w++) {
-				System.out.println("place[" + w + "] : " + place[w]);
-			}
 			Arrays.sort(place);
+			
 			answer[i] = place[z];
-			System.out.println("place["+ z + "]: " + place[z]);
+			System.out.println("¾Æ·¡ place["+ z + "]: " + place[z]);
 			System.out.println("answer[" + i + "] : " + answer[i]);
 		}
 		
