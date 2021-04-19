23a84fe72a7c9b3a6526133bb538570c7ad59b7a
kimchang0 <newshfkfk@gmail.com>
2021-04-19 09:10:14 +0900

    210419_Programers_make_prime_number_start

---
+++ Study/src/kcy/Programers_make_prime_number.java
@@ -0,0 +1,16 @@
+package kcy;
+
+public class Programers_make_prime_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int solution(int[] nums) {
+		int answer = -1;
+		
+		return answer;
+	}
+
+}
