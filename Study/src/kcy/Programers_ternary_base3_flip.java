bbbb753baa9852e2794b106075f82476e3e1a015
kimchang0 <newshfkfk@gmail.com>
2021-04-23 17:36:29 +0900

    210423_Programers_ternary_base3_flip_success

--- Study/src/kcy/Programers_ternary_base3_flip.java
+++ Study/src/kcy/Programers_ternary_base3_flip.java
@@ -2,28 +2,33 @@ package kcy;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Programers_ternary_base3_flip {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
         
-        int quotient = 15;
-        int cardinalNum = 3;
-        List<Integer> output = new ArrayList<>();
-        
-        while(quotient != 0) {
-            output.add(quotient%cardinalNum); // 나머지가 저장됨
-            quotient /= cardinalNum;
-        }
-        
-        System.out.println(output);
+
+		int quotient = 15;
+		System.out.println(solution(quotient));
     }
 	
-    public int solution(int n) {
+    public static int solution(int n) {
         int answer = 0;
         
+        List<Integer> output = new ArrayList<>();
+        
+        while(n != 0) {
+            output.add(n % 3); // 나머지가 저장됨
+            n /= 3;
+        }
+        int three = 1;
+        System.out.println(output);
+        for(int i = output.size()-1; i >= 0; i--) {
+        	answer += three * output.get(i);
+        	System.out.println(answer);
+        	three *= 3;
+        }
         
         return answer;
     }
