1790c4b47d538416b186a4674fe8bbd2d4b74eed
kimchang0 <newshfkfk@gmail.com>
2021-04-26 22:15:25 +0900

    210426_Programers_caesar_cipher_trying

--- Study/src/kcy/Programers_caesar_cipher.java
+++ Study/src/kcy/Programers_caesar_cipher.java
@@ -4,8 +4,8 @@ public class Programers_caesar_cipher {
 
 	public static void main(String[] args) {
 		
-		String str = "AB";
-		int num = 1;
+		String str = "a B z";
+		int num = 4;
 		System.out.println(solution(str, num));
 		
 	}
@@ -14,10 +14,33 @@ public class Programers_caesar_cipher {
         String answer = "";
         
         String[] arr = s.split("");
+        boolean cases = false;
         
         for(String a : arr) {
         	char cvt = a.charAt(0);
-        	cvt += n;
+        	if((int)cvt <= 90) {
+        		cases = false;
+        	} else {
+        		cases = true;
+        	}
+        	System.out.println("n + before cvt: " + cvt);
+        	if(cvt != ' ') {
+        		cvt += n;
+        	}
+        	System.out.println("n + after cvt: " + cvt);
+        	
+        	if(cases) {
+        		if((int)cvt > 122) {
+        			System.out.println((int)cvt);
+        			cvt -= 26;
+        		}
+        	} else {
+        		if((int)cvt > 91) {
+        			cvt -= 26;
+        		}
+        		
+        	}
+        	System.out.println(cvt);
         	answer += cvt;
         }
         
