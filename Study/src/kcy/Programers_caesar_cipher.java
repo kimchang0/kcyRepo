51a10801c844993444fe668eba941faeeb987245
kimchang0 <newshfkfk@gmail.com>
2021-04-27 23:16:01 +0900

    210427_Programers_caesar_cipher_success

--- Study/src/kcy/Programers_caesar_cipher.java
+++ Study/src/kcy/Programers_caesar_cipher.java
@@ -5,7 +5,7 @@ public class Programers_caesar_cipher {
 	public static void main(String[] args) {
 		
 		String str = "a B z";
-		int num = 4;
+		int num = 25;
 		System.out.println(solution(str, num));
 		
 	}
@@ -23,24 +23,21 @@ public class Programers_caesar_cipher {
         	} else {
         		cases = true;
         	}
-        	System.out.println("n + before cvt: " + cvt);
+        	
         	if(cvt != ' ') {
         		cvt += n;
         	}
-        	System.out.println("n + after cvt: " + cvt);
         	
         	if(cases) {
-        		if((int)cvt > 122) {
-        			System.out.println((int)cvt);
+        		while((int)cvt > 122) {
         			cvt -= 26;
         		}
         	} else {
-        		if((int)cvt > 91) {
+        		while((int)cvt > 90) {
         			cvt -= 26;
         		}
         		
         	}
-        	System.out.println(cvt);
         	answer += cvt;
         }
         
