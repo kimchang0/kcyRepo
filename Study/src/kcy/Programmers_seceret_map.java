1537bf73a2855701050148f4059d3918b406090b
kimchang0 <newshfkfk@gmail.com>
2021-05-01 12:47:01 +0900

    210501_Programmers_seceret_map_success

--- Study/src/kcy/Programmers_seceret_map.java
+++ Study/src/kcy/Programmers_seceret_map.java
@@ -5,44 +5,84 @@ import java.util.Arrays;
 public class Programmers_seceret_map {
 
 	public static void main(String[] args) {
+
+		// Integer.Binary 2진수 변환, toString으로 변환, split으로 나누고, 같으면 # 아니면 " "
+		int[] arr1 = { 46, 33, 33 ,22, 31, 50 };
+		int[] arr2 = { 27 ,56, 19, 14, 14, 10 };
+		int n = 6;
+		System.out.println(Arrays.toString(solution1(n, arr1, arr2)));
 		
-		//Integer.Binary 2진수 변환, toString으로 변환, split으로 나누고, 같으면 # 아니면 " "
-		int[] arr1 = {9, 20, 28, 18, 11};
-		int[] arr2 = {30, 1, 21, 17, 28};
-		int n = 5;
-		System.out.println(Arrays.toString(solution(n, arr1, arr2)));
+		//다른 사람의 풀이는 공간복잡도는 좋지만 시간복잡도가 안좋고
+		//내 풀이는 공간복잡도는 안좋지만 시간복잡도가 좋다.
+		//둘 다 해결할 솔루션 생각해보자
 	}
-    public static String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        boolean[] map1 = new boolean[n];
-        boolean[] map2 = new boolean[n];
-        
-        
-        for(int i = 0; i < arr1.length; i++) {
+	//다른 사람의 풀이
+	public static String[] solution(int n, int[] arr1, int[] arr2) {
+		String[] answer = new String[n];
 
-        	if((Integer.toBinaryString(arr1[i])).equals("1")){
-        		map1[i] = true;
-        	} else {
-        		map1[i] = false;
-        	}
+		for (int i = 0; i < n; i++) {
+			answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+		}
 
-        	if((Integer.toBinaryString(arr2[i])).equals("1")){
-        		map2[i] = true;
-        	} else {
-        		map2[i] = false;
-        	}
-        	
-        	answer[i] += map1[i] == true && map2[i] == true ? "#" : " ";
-        }
-        
+		for (int i = 0; i < n; i++) {
+			answer[i] = String.format("%" + n + "s", answer[i]);
+			answer[i] = answer[i].replaceAll("1", "#");
+			answer[i] = answer[i].replaceAll("0", " ");
+		}
 
-        for(int i = 0; i < map1.length; i++) {
-        	System.out.println(map1[i]);
-        	System.out.println(map2[i]);
-        	System.out.println(answer[i]);
-        }
-        
-        return answer;
-    }
+		return answer;
+	}
+	//내 풀이
+	public static String[] solution1(int n, int[] arr1, int[] arr2) {
+		String[] answer = new String[n];
+		boolean[] map1 = new boolean[n];
+		boolean[] map2 = new boolean[n];
+
+		for (int i = 0; i < arr1.length; i++) {
+
+			String temp1 = Integer.toBinaryString(arr1[i]);
+			String temp2 = Integer.toBinaryString(arr2[i]);
+			if (temp1.length() <= n) {
+				for (int j = temp1.length(); j < n; j++) {
+					temp1 = "0" + temp1;
+				}
+			}
+			if (temp2.length() <= n) {
+				for (int j = temp2.length(); j < n; j++) {
+					temp2 = "0" + temp2;
+				}
+			}
+			String[] array1 = temp1.split("");
+			String[] array2 = temp2.split("");
+			for (int j = 0; j < array1.length; j++) {
+				if (array1[j].equals("1")) {
+					map1[j] = true;
+				} else {
+					map1[j] = false;
+				}
+
+				if (array2[j].equals("1")) {
+					map2[j] = true;
+				} else {
+					map2[j] = false;
+				}
+				if (j == 0) {
+					if (map1[j] == true || map2[j] == true) {
+						answer[i] = "#";
+					} else {
+						answer[i] = " ";
+					}
+				} else {
+					if (map1[j] == true || map2[j] == true) {
+						answer[i] += "#";
+					} else {
+						answer[i] += " ";
+					}
+				}
+			}
+
+		}
+		return answer;
+	}
 
 }
