19102ea5243c993b0ef842faac2414af9f0ad6c1
kimchang0 <newshfkfk@gmail.com>
2021-05-05 17:46:47 +0900

    210505_Programmers_Recommend_new_ID_success

---
+++ Study/src/kcy/Programmers_Recommend_new_ID.java
@@ -0,0 +1,37 @@
+package kcy;
+
+public class Programmers_Recommend_new_ID {
+
+	public static void main(String[] args) {
+
+		String str = "";
+		System.out.println(solution(str));
+
+	}
+
+	public static String solution(String new_id) {
+
+		new_id = new_id.toLowerCase();
+		new_id = new_id.replaceAll("[^0-9a-z._-]", "");
+		new_id = new_id.replaceAll("\\.\\.*", ".");
+		new_id = new_id.replaceAll("^\\.", "");
+		new_id = new_id.replaceAll("\\.$", "");
+		if(new_id.equals("") || new_id.equals(" ")) {
+			new_id = "a";
+		}
+		if(new_id.length() > 15) {
+			new_id = new_id.substring(0,15);
+		}
+		new_id = new_id.replaceAll("\\.$", "");
+		if(new_id.length() <= 2) {
+			while(true) {
+				new_id += new_id.substring(new_id.length()-1);
+				if(new_id.length() == 3) {
+					break;
+				}
+			}
+		}
+		
+		return new_id;
+	}
+}
