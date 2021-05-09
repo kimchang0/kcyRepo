e8f572c9dbc483f04e3fe2fe398776649ab94d16
kimchang0 <newshfkfk@gmail.com>
2021-05-09 18:08:51 +0900

    210509_Programmers_dart_game_success

--- Study/src/kcy/Programmers_dart_game.java
+++ Study/src/kcy/Programmers_dart_game.java
@@ -1,45 +1,60 @@
 package kcy;
 
+import java.util.Arrays;
+
 public class Programmers_dart_game {
 
 	public static void main(String[] args) {
-		
-		System.out.println(solution("1S2D*3T"));
-		
+
+		System.out.println(solution("1D2S3T*"));
+
 	}
-	
-    public static int solution(String dartResult) {
-        int answer = 0;
-        int last_score = 0;
-        String[] arr = dartResult.split("");
-        for(int i = 0; i < arr.length; i++) {
-        	if(arr[i].equals("S")) {
-        		arr[i] =  "x1 ";
-        	} else if (arr[i].equals("D")) {
-        		arr[i] =  "x2 ";
-        	} else if (arr[i].equals("T")) {
-        		arr[i] =  "x3 ";
-        	} else if (arr[i].equals("*")) {
-        		arr[i] = "* ";
-        	} else if (arr[i].equals("#")) {
-        		arr[i] = "# ";
-        	}
-        }
-        String temp = String.join("", arr);
-        arr = null;
-        arr = temp.split(" ");
-        temp = null;
-        for(int i = 0; i < arr.length; i++) {
-        	String[] numbers = arr[i].split("x");
-        	int temp_num = 1;
-        	System.out.println("numbers[i]: " + numbers[1]);
-        	for(int j = 1; j < Integer.parseInt(numbers[1]); j++) {
-        		temp_num *= Integer.parseInt(numbers[0]);
-        	}
-        	
-        }
-        
-        return answer;
-    }
+
+	public static int solution(String dartResult) {
+		int[] last_score = new int[3];
+		String[] arr = dartResult.split("");
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equals("S")) {
+				arr[i] = "x1 ";
+			} else if (arr[i].equals("D")) {
+				arr[i] = "x2 ";
+			} else if (arr[i].equals("T")) {
+				arr[i] = "x3 ";
+			} else if (arr[i].equals("*")) {
+				arr[i] = "* ";
+			} else if (arr[i].equals("#")) {
+				arr[i] = "# ";
+			}
+		}
+		String temp = String.join("", arr);
+		arr = null;
+		arr = temp.split(" ");
+		temp = null;
+		int k = 0;
+		for (int i = 0; i < arr.length; i++) {
+			String[] numbers = arr[i].split("x");
+			int temp_num = 1;
+
+			if (numbers[0].equals("*")) {
+				if (k-1 == 0) {
+					last_score[k-1] *= 2;
+				} else {
+					last_score[k - 2] *= 2;
+					last_score[k - 1] *= 2;
+				}
+			} else if (numbers[0].equals("#")) {
+				last_score[k - 1] *= -1;
+			}
+			if (!(numbers[0].equals("*") || numbers[0].equals("#"))) {
+				for (int j = 1; j <= Integer.parseInt(numbers[1]); j++) {
+					temp_num *= Integer.parseInt(numbers[0]);
+				}
+				last_score[k] = temp_num;
+				k++;
+			}
+
+		}
+		return last_score[0] + last_score[1] + last_score[2];
+	}
 
 }
