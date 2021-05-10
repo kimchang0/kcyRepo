8b4ce10f5f200d0ead748e20c1d66d5c0f0bf457
kimchang0 <newshfkfk@gmail.com>
2021-05-10 23:58:03 +0900

    210510_Programmers_failure_rate_start

---
+++ Study/src/kcy/Programmers_failure_rate.java
@@ -0,0 +1,17 @@
+package kcy;
+
+public class Programmers_failure_rate {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        
+        
+        return answer;
+    }
+}
