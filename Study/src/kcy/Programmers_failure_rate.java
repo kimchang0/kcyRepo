05c8096cd82dafd882acb9de8a2d6bfb1348cc66
kimchang0 <newshfkfk@gmail.com>
2021-05-11 14:50:45 +0900

    210511_Programmers_failure_rate_trying

--- Study/src/kcy/Programmers_failure_rate.java
+++ Study/src/kcy/Programmers_failure_rate.java
@@ -1,5 +1,7 @@
 package kcy;
 
+import java.util.HashMap;
+
 public class Programmers_failure_rate {
 
 	public static void main(String[] args) {
@@ -11,6 +13,21 @@ public class Programmers_failure_rate {
     public int[] solution(int N, int[] stages) {
         int[] answer = {};
         
+        for(int i = 0; i < N; i++) {
+        	for(int j = 0; j < stages.length; j++) {
+            	if(stages[j] == i && stages[j] < i) {
+            		
+            	}
+        	}
+        }
+        HashMap<String, Integer> duplicate_count = new HashMap<String, Integer>();
+        for(int i = 0 ; i < duplicate_count.size() ; i++){
+            if (duplicate_count.containsKey(duplicate_count.get(i))) {
+                duplicate_count.put(duplicate_count.get(i), duplicate_count.get(duplicate_data.get(i))  + 1);
+            } else {
+                duplicate_count.put(duplicate_count.get(i) , 1);
+            }
+        }
         
         return answer;
     }
